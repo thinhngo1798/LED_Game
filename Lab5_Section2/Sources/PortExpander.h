4c96930d5082ead1090766b6d5013c407709f0cc
Chuong Ho <chuong,git@gmail.com>
2019-10-21 20:02:01 +1100

    Adding pin name

--- Lab5_Section2/Sources/PortExpander.h
+++ Lab5_Section2/Sources/PortExpander.h
@@ -11,6 +11,29 @@
 //1: transmit (output)
 //0: receive (input)
 
+//Input/Output Direction
+static const uint8_t IODIR_ADDRESS = 0x00<<1;
+
+static const uint8_t IPOL_ADDRESS = 0x01<<1;
+
+static const uint8_t GPINTEN_ADDRESS = 0x02<<1;
+
+static const uint8_t DEFVAL_ADDESS = 0x03<<1;
+
+static const uint8_t INTCON_ADDRESS = 0x04<<1;
+
+static const uint8_t IOCON_ADDRESS = 0x05<<1;
+
+static const uint8_t GPPU_ADDRESS = 0x06<<1;
+
+static const uint8_t INTF_ADDRESS = 0x07<<1;
+
+static const uint8_t INTCAP_ADDRESS = 0x08<<1;
+
+static const uint8_t GPIO_ADDRESS = 0x09<<1;
+
+static const uint8_t OLAT_ADDRESS = 0x0A<<1;
+
 void setDirection(uint8_t direction);
 
 //think about adding function to setting individual bit/clear bit
