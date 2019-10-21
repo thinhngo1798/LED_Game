ecac5ac61d75958a567965fc295b359451163583
Chuong Ho <41665273+chgswin@users.noreply.github.com>
2019-10-21 20:42:29 +1100

    Update PortExpander.h

--- Lab5_Section2/Sources/PortExpander.h
+++ Lab5_Section2/Sources/PortExpander.h
@@ -14,8 +14,24 @@
 //Input/Output Direction
 static const uint8_t IODIR_ADDRESS = 0x00<<1;
 
+//Input Polarity
 static const uint8_t IPOL_ADDRESS = 0x01<<1;
 
+//Pull-up configuration
+static const uint8_t GPPU_ADDRESS = 0x06<<1;
+
+//Port (GPIO) register
+//reading from this register reads the port.
+//writing to this register modifies the OUTPUT latch.
+static const uint8_t GPIO_ADDRESS = 0x09<<1;
+
+//a write to this register modifies the pins configured as outputs.
+static const uint8_t OLAT_ADDRESS = 0x0A<<1;
+
+
+/*
+* may not be neccessary but not completely forgetting
+//Interrupt: may not need this
 static const uint8_t GPINTEN_ADDRESS = 0x02<<1;
 
 static const uint8_t DEFVAL_ADDESS = 0x03<<1;
@@ -24,15 +40,11 @@ static const uint8_t INTCON_ADDRESS = 0x04<<1;
 
 static const uint8_t IOCON_ADDRESS = 0x05<<1;
 
-static const uint8_t GPPU_ADDRESS = 0x06<<1;
-
 static const uint8_t INTF_ADDRESS = 0x07<<1;
 
 static const uint8_t INTCAP_ADDRESS = 0x08<<1;
+*/
 
-static const uint8_t GPIO_ADDRESS = 0x09<<1;
-
-static const uint8_t OLAT_ADDRESS = 0x0A<<1;
 
 void setDirection(uint8_t direction);
 
