81bef4b469d0a2f6c6836b66f0cbb21dfc84aa74
Thinh <thinhngo.1798@gmail.com>
2019-10-29 23:41:22 +1100

    Some first thoughts

--- Lab5_Section2/Sources/i2c-example.cpp
+++ Lab5_Section2/Sources/i2c-example.cpp
@@ -8,7 +8,7 @@
  ============================================================================
  */
 #include "i2c.h"
-
+#include "PortExpander.h"
 using namespace USBDM;
 
 // Address (LSB = R/W bit)
@@ -26,7 +26,28 @@ int main() {
    I2c0 i2c{I2C_SPEED, I2cMode_Polled};
 
    for(;;) {
-	   console.writeln("ALL GOOD!");
+	   unsigned numberOfInput;
+	   unsigned choice[8];
+	   console.writeln("Please enter the number of input in 8-bits: ");
+	   console.readln(numberOfInput);
+	   for (int i=0;i<numberOfInput;i++)
+	   {
+	   console.writeln("Please input the position of your input right to left from 0 to 7th");
+			   //a 8-bit unsigned integer indicating the preferable direction (0 for transmitting and 1 for receiving):");
+	   console.readln(choice[i]);
+	   }
+	   uint8_t Mask =0b00000000;
+	   for (int l=0;l<numberOfInput;l++)
+	   {
+		   Mask |= 1<<choice[l];
+	   }
+	   setDirection(Mask);
+	   //All the output LED will be on;
+	   write(0b11111111);
+
+	   //All output will be off and then capture the input for testing.
+	   write(0b00000000);
+
 //      {
 //         /*
 //          * Transmits 4 bytes and receives 2 bytes using different buffers
@@ -39,17 +60,17 @@ int main() {
 //         i2c.txRx(I2C_ADDRESS, sizeof(txData), txData, sizeof(rxData), rxData);
 //         i2c.endTransaction();
 //      }
-      {
-         /*
-          * Transmits 2 bytes and receives 4 bytes into same buffer
-          * This will use a repeated start at turnover
-          */
-         uint8_t data[4] = { 0xA1,0xB2, };
-
-         i2c.startTransaction();
-         i2c.txRx(I2C_ADDRESS, 2, sizeof(data), data);
-         i2c.endTransaction();
-      }
+//      {
+//         /*
+//          * Transmits 2 bytes and receives 4 bytes into same buffer
+//          * This will use a repeated start at turnover
+//          */
+//         uint8_t data[4] = { 0xA1,0xB2, };
+//
+//         i2c.startTransaction();
+//         i2c.txRx(I2C_ADDRESS, 2, sizeof(data), data);
+//         i2c.endTransaction();
+//      }
 //      {
 //         /*
 //          * Transmits 4 bytes
