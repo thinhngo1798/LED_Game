998873536c5c60966c64541ece8a09194b938e77
Chuong Ho <41665273+chgswin@users.noreply.github.com>
2019-10-31 10:17:28 +1100

    Update PortExpander.h

--- Lab5_Section2/Sources/PortExpander.h
+++ Lab5_Section2/Sources/PortExpander.h
@@ -14,30 +14,10 @@
 //Input/Output Direction
 
 enum RegAddress {
-	RegAddress_IODIR = 0x00<<1,
-	RegAddress_GPIO = 0x09<<1,
-	RegAddress_OLAT = 0x0A<<1
+	RegAddress_IODIR = 0x00,
+	RegAddress_GPIO = 0x09,
+	RegAddress_OLAT = 0x0A
 };
-
-//static const uint8_t IODIR_ADDRESS = 0x00<<1;
-//
-//static const uint8_t IPOL_ADDRESS = 0x01<<1;
-//
-//static const uint8_t GPINTEN_ADDRESS = 0x02<<1;
-//
-//static const uint8_t DEFVAL_ADDESS = 0x03<<1;
-//
-//static const uint8_t INTCON_ADDRESS = 0x04<<1;
-//
-//static const uint8_t IOCON_ADDRESS = 0x05<<1;
-//
-//static const uint8_t GPPU_ADDRESS = 0x06<<1;
-//
-//static const uint8_t INTF_ADDRESS = 0x07<<1;
-//
-//static const uint8_t INTCAP_ADDRESS = 0x08<<1;
-
-
 
 void writeRegister(RegAddress regAddress, uint8_t data);
 
