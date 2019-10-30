5e772232dba9b7c754710912a4966f0b3aa886a0
Chuong Ho <41665273+chgswin@users.noreply.github.com>
2019-10-31 10:03:29 +1100

    Update PortExpander.cpp

--- Lab5_Section2/Sources/PortExpander.cpp
+++ Lab5_Section2/Sources/PortExpander.cpp
@@ -4,24 +4,20 @@
  *  Created on: Oct 25, 2019
  *      Author: PLH
  */
-
 #include "i2c.h"
 
 #include "PortExpander.h"
 
 using namespace USBDM;
 
-	 static const unsigned I2C_SPEED   = 400*kHz;
-
-static const unsigned I2C_ADDRESS = 0x1D<<1;
-
+static const unsigned I2C_SPEED   = 400*kHz;
+static const unsigned I2C_ADDRESS = 0x20<<1;
 
 I2c0 i2c{I2C_SPEED, I2cMode_Polled};
 
-
 void writeRegister(RegAddress regAddress, uint8_t data)
 {
-	uint8_t arraySent[] = {regAddress, data};
+	const uint8_t arraySent[] = {regAddress, data};
 	i2c.startTransaction();
 	i2c.transmit(I2C_ADDRESS, sizeof(arraySent), arraySent);
 	i2c.endTransaction();
@@ -29,8 +25,8 @@ void writeRegister(RegAddress regAddress, uint8_t data)
 
 uint8_t readRegister(RegAddress regAddress)
 {
-	uint8_t arrayTx[] = {regAddress};
-	uint8_t arrayRx[2];
+	const uint8_t arrayTx[] = {regAddress};
+	uint8_t arrayRx[1];
 	i2c.startTransaction();
 	i2c.txRx(I2C_ADDRESS, sizeof(arrayTx), arrayTx, sizeof(arrayRx), arrayRx);
 	i2c.endTransaction();
@@ -52,3 +48,9 @@ uint8_t read()
 	return readRegister(RegAddress_GPIO);
 }
 
+void setPinDirection(uint8_t pinNumber)
+{
+	uint8_t directionValue = readRegister(RegAddress_IODIR);
+	directionValue |= (1<<pinNumber);
+	setDirection(directionValue);
+}
