8f0266f509c24e30b83b7972d5c3324fc6740da6
Chuong Ho <41665273+chgswin@users.noreply.github.com>
2019-10-31 11:42:53 +1100

    Create testing.h

---
+++ Lab5_Section2/Sources/testing.h
@@ -0,0 +1,33 @@
+/*
+ * testing.h
+ *
+ *  Created on: Oct 31, 2019
+ *      Author: PLH
+ */
+
+#ifndef SOURCES_TESTING_H_
+#define SOURCES_TESTING_H_
+
+#include "hardware.h"
+#include "i2c.h"
+#include "pit.h"
+#include "smc.h"
+#include "PortExpander.h"
+
+using namespace USBDM;
+
+//
+//Unit level testing
+//
+void testSetDirection();
+void testRead();
+void testWrite();
+
+//
+//System level testing
+//
+void testSystem();
+
+
+
+#endif /* SOURCES_TESTING_H_ */
