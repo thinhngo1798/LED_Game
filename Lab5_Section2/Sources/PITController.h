c2313ca786bf7499f89fdc82d935830eac027fcb
Chuong Ho <41665273+chgswin@users.noreply.github.com>
2019-11-03 02:05:31 +1100

    Create PITController.h

---
+++ Lab5_Section2/Sources/PITController.h
@@ -0,0 +1,16 @@
+/*
+ * PITController.h
+ *
+ *  Created on: Nov 3, 2019
+ *      Author: PLH
+ */
+
+#ifndef SOURCES_PITCONTROLLER_H_
+#define SOURCES_PITCONTROLLER_H_
+
+void initialisePIT();
+
+void resetGame();
+
+
+#endif /* SOURCES_PITCONTROLLER_H_ */
