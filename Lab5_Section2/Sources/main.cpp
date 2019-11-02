75862db323cd4dea80e01dd41717a7885bb871d3
Chuong Ho <41665273+chgswin@users.noreply.github.com>
2019-11-03 02:11:30 +1100

    Update main.cpp

--- Lab5_Section2/Sources/main.cpp
+++ Lab5_Section2/Sources/main.cpp
@@ -1,10 +1,11 @@
 /*
  ============================================================================
- * @file    i2c-example.cpp (180.ARM_Peripherals/Snippets)
- * @brief   Basic C++ demo of using I2C interface
+ * @file    main.cpp
+ * @brief   Mainline for the Accelerometer LED game
  *
- *  Created on: 10/6/2017
- *      Author: podonoghue
+ *  Created on: 17/10/19
+ *      Author: Dac Thanh Chuong Ho
+ *      Co-autor: Quang Thinh Ngo
  ============================================================================
  */
 
@@ -20,20 +21,24 @@ using namespace USBDM;
 using StartSwitch = GpioC<0, ActiveLow>;
 
 int main() {
+	//Switch to start the game
 	StartSwitch::setInput(PinPull_Up, PinAction_None, PinFilter_None);
 	bool lastSwitchValue = false;
 	bool currentSwitchValue;
-   // Declare I2C interface
+
+	// Set up the PIT
    initialisePIT();
+   //Set all pins as outputs.
    setDirection(0b00000000);
 
    for(;;)
    {
 	   currentSwitchValue = StartSwitch::isPressed();
-
+	   //Detect when the Switch is pressed to restart the game.
 	   if (currentSwitchValue && !lastSwitchValue)
 	   {
 		   console.writeln("Game starts!");
+		   //set up the game again.
 		   resetGame();
 	   }
 	   lastSwitchValue = currentSwitchValue;
